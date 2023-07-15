287ee3dca5e9316d1f6d000f89e46902f1d42098
Ziad Ehsan <ziadehsan4.0@gmail.com>
2023-07-15 12:06:32 +0600

    Calculates a triangel's area based n height and base

---
+++ triange_area_calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+float base;
+float height;
+cout<<"Enter base: ";
+cin>>base;
+
+cout<<"Enter height: ";
+cin>>height;
+
+cout<<"The area is " << 0.5 * base * height;
+
+}
